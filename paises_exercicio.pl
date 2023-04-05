9ae7017f548890751401bd9192e6907c80020084
BotelhoMDS <67609009+BotelhoMDS@users.noreply.github.com>
2023-04-04 22:46:33 -0300

    Update paises_exercicio.pl

--- paises_exercicio.pl
+++ paises_exercicio.pl
@@ -22,7 +22,7 @@ pais(india,aisa,1408).
 pais(russia,asia,143).
 pais(russia,europa,143).
 pais(eua,america,330).
-pais(franca,america,0.2). %Guiana Francesa
+pais(franca,america,0.2). %Guiana Francesa,guadalupe,martinica
 pais(brasil,america,250). 
 pais(colombia,america,50). 
 pais(argentina,america,45). 
@@ -35,6 +35,27 @@ pais(paraguai,america,7).
 pais(uruguai,america,3). 
 pais(guiana,america,0.7). 
 pais(suriname,america,0.5).
+pais(mexico,america,127). 
+pais(canada,america,38). 
+pais(honduras,america,10). 
+pais(guatemala,america,17). 
+pais(belize,america,0.4). 
+pais(el_salvador,america,6).
+pais(cuba,america,11). 
+pais(costa_rica,america,5). 
+pais(panama,america,4). 
+pais(nicaragua,america,6). 
+pais(jamaica,america,3). 
+pais(haiti,america,11). 
+pais(republica_dominicana,america,11). 
+pais(porto_rico,america,3). 
+pais(aruba,america,0.1). 
+pais(barbados,america,0.2). 
+pais(curacao,america,0.1). 
+pais(granada,america,0.1). 
+pais(trindade_e_tobago,america,1). 
+pais(santa_lucia,america,0.1). 
+
 
 
 %definição das fronteiras de um pais
@@ -50,6 +71,18 @@ fronteira(belgica,holanda).
 fronteira(alemanha,holanda). 
 fronteira(alemanha,franca). 
 fronteira(china,mongolia). 
+fronteira(brasil,uruguai). 
+fronteira(brasil,argentina). 
+fronteira(brasil,bolivia). 
+fronteira(brasil,paraguai). 
+fronteira(brasil,colombia). 
+fronteira(brasil,suriname). 
+fronteira(brasil,venezuela). 
+fronteira(brasil,guiana). 
+fronteira(argentina,bolivia). 
+fronteira(argentina,uruguai). 
+fronteira(argentina,chile). 
+fronteira(argentina,paraguai). 
 
 juntos(Xpais,Ypais) :- fronteira(Xpais,Ypais);fronteira(Ypais,Xpais). 
 paises_continente(Paises,Continente) :- findall(Pais,pais(Pais,Continente,_),Paises).
